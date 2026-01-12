08a4f5985535eed0d88edfbb5f05d07539172735
Nicolaas du Toit van der Merwe <nicolaasvdmerwe0@gmail.com>
2026-01-12 12:07:03 +0200

    Basic Movement Animations
    Implemented idle and walking animations for the player using blend trees.

--- StudioBoInternship/Assets/Scripts/PlayerMovement.cs
+++ StudioBoInternship/Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,14 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
+    private Animator _animator;
     [SerializeField] private float movementSpeed = 5f;
     private Vector2 _movementInput;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -19,6 +21,15 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        _animator.SetBool("isWalking", true);
+        if (context.canceled)
+        {
+            _animator.SetBool("isWalking", false);
+            _animator.SetFloat("LastInputX", _movementInput.x);
+            _animator.SetFloat("LastInputY", _movementInput.y);
+        }
         _movementInput = context.ReadValue<Vector2>();
+        _animator.SetFloat("InputX", _movementInput.x);
+        _animator.SetFloat("InputY", _movementInput.y);
     }
 }
